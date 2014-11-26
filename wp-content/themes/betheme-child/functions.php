cf6b7ac357658a93a69d097b2762835cdfe63d24
maros <maros@MPcom>
2014-11-25 21:00:52 -0400

    initial mo

--- wp-content/themes/betheme-child/functions.php
+++ wp-content/themes/betheme-child/functions.php
@@ -20,7 +20,8 @@ function netflix($atts) {
     // WP_Query arguments
     $args = array(
         'post_type' => 'activity',
-        'category_name' => '',
+        'category_name' => $atts['category'],
+        //'posts_per_page'=> -1
         'posts_per_page'=> -1
     );
 
@@ -28,9 +29,9 @@ function netflix($atts) {
     $myquery = new WP_Query($args);
     if ($myquery->have_posts()) {
         $code = '<div class="slider">
-                    <div id="left1" class="left">&lt;</div>
-                    <div id="right1" class="right">&gt;</div>
-                    <div class="track" id="track1">';
+                    <div class="moveit left"><i class="fa fa-caret-left"></i></div>
+                    <div class="moveit right"><i class="fa fa-caret-right"></i></div>
+                    <div class="track">';
         while ($myquery->have_posts()) {
             $myquery->the_post();
             $img_id = get_post_thumbnail_id($post->ID); // This gets just the ID of the img
--- wp-content/themes/betheme-child/js/mp_js.js
+++ wp-content/themes/betheme-child/js/mp_js.js
@@ -1,7 +1,9 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
     //get track width
+
+    //test2
     function track_widths() {
-        $('.track').each(function () {
+        $('.track').each(function() {
             var items = $(this).children('.box').length;
             var length = items * 400;
             $(this).css("width", length);
@@ -10,16 +12,22 @@ jQuery(document).ready(function ($) {
     ;
     track_widths();
     function iniboxposition() {
-        $('.track .box').each(function (index) {
-            $(this).addClass('boxno' + index);
-            var elem = $(this);
-            initialpos = 400 * index;
-            //console.log(initialpos);
-            $(elem).css('left', initialpos);
+        $('.track').each(function() {
+            $(this).children('.box').each(function(index) {
+                $(this).addClass('boxno' + index);
+                var elem = $(this);
+                initialpos = 400 * index;
+                //console.log(initialpos);
+                $(elem).css('left', initialpos);
+            });
         });
     }
     iniboxposition();
-
+$('.moveit').hover(function () {
+    $(this).addClass('hover');
+}, function () {
+    $(this).removeClass('hover');
+});
 //    setInterval(function () {
 //        $('.track .box').each(function (index) {
 //            var elem = $('boxno' + index);
@@ -32,18 +40,35 @@ jQuery(document).ready(function ($) {
 //            }
 //        });
 //    }, 10);
+    function reposition_boxes() {
+        $('.track').each(function() {
+            var items = $(this).children('.box').length;
+            $(this).children('.box').each(function(index) {
+                //console.log(index);
+                var elem = $('.boxno' + index);
+                var left1 = parseInt($(elem).css('left'));
+                //var items = $('.track').children('.box').length;
+                var length = (items) * 400;
+                if (left1 < -400) {
+                    left1 = left1 + length;
+                }
+                ;
+                if (left1 > (length - 400)) {
+                    left1 = left1 - length;
+                }
+                ;
+                if ($(this).parent('.slider').children('.left.hover').length > 0) {
+                    $(elem).css('left', left1 - 4);
+                }
+                else if ($(this).parent('.slider').children('.right.hover').length > 0) {
+                    $(elem).css('left', left1 + 4);
+                }
+            });
+        });
+    }
 
-
-    setInterval(function () {
+    setInterval(function() {
         //console.log(index);
-        var elem = $('.boxno0');
-        $(elem).css('left', left1 - 2);
-        var left1 = parseInt($(elem).css('left'));
-        if ($('#left1:hover').length > 0) {
-            $(elem).css('left', left1 - 2);
-        }
-        else if ($('#right1:hover').length > 0) {
-            $(elem).css('left', left1 + 2);
-        }
+        reposition_boxes();
     }, 10);
 });
